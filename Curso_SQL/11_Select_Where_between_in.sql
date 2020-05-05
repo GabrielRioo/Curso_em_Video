50361e88cebab530b158f4ef135ae741fe7e6436
GabrielRioo <Gabrielpaivario@gmail.com>
2020-05-05 15:52:22 -0300

    Adding the Commands of 'aula 11'

---
+++ Curso_SQL/11_Select_Where_between_in.sql
@@ -0,0 +1,15 @@
+SELECT * FROM cursos
+ORDER BY nome
+
+SELECT * FROM cursos
+ORDER BY nome desc
+
+SELECT nome, descricao, carga FROM cursos
+ORDER BY nome
+
+SELECT * FROM cursos 
+WHERE ano = '2016'
+ORDER BY nome
+
+SELECT * FROM cursos
+WHERE ano BETWEEN '2017' and '2018'

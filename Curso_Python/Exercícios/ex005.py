6066919a17217d3dbb512a0b319c11677f192c45
GabrielRioo <Gabrielpaivario@gmail.com>
2020-05-21 12:10:18 -0300

    Adding the exercise 005

---
+++ Curso_Python/Exercícios/ex005.py
@@ -0,0 +1,5 @@
+# Faça um programa que leia um numero inteiro e mostre na tela o seu sucessor e seu antecessor.
+n = int(input('Digite um número: '))
+n2 = n + 1
+n3 = n - 1
+print('seu sucessor é {0} e seu antecessor é {1}'.format(n2, n3))

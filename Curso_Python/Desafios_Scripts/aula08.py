d23048aa1a6d598dcb5090f8416ffb159bc09bb5
GabrielRioo <Gabrielpaivario@gmail.com>
2020-05-21 21:06:29 -0300

    Adding the class 08

---
+++ Curso_Python/Desafios_Scripts/aula08.py
@@ -0,0 +1,12 @@
+# from math import floor, sqrt
+#
+# num = int(input('Digite um numero: '))
+# raiz = sqrt(num)
+# print('A raiz quadrada de {} é igual a {}'.format(num, floor(raiz)))
+
+# import random
+# num = random.randint(1, 10)
+# print(num)
+
+import emoji
+print(emoji.emojize('Olá mundo :blue_heart: :cry:', use_aliases=True))

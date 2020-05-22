fe36805b402aa36fff03668f55e45c8cbf3559a3
GabrielRioo <Gabrielpaivario@gmail.com>
2020-05-22 09:50:49 -0300

    Adding the exercise 020

--- Curso_Python/Exercícios/ex020.py
+++ Curso_Python/Exercícios/ex020.py
@@ -1,1 +1,12 @@
-# O mesmo professor do desafio anterior quer sortear a ordem de apresentação de trabalhos dos alunos. Faça um programa que leia o nome dos quatro alunos e mostre a ordem sorteada.
+# O mesmo professor do desafio anterior quer sortear a ordem de apresentação de trabalhos dos alunos.
+# Faça um programa que leia o nome dos quatro alunos e mostre a ordem sorteada.
+
+from random import shuffle
+
+n1 = str(input('Digite o primeiro aluno: '))
+n2 = str(input('Digite o segundo nome: '))
+n3 = str(input('Digite o terceiro nome: '))
+n4 = str(input('Digite o quarto nome: '))
+lista = [n1, n2, n3, n4]
+escolhidos = shuffle(lista)
+print('A ordem de apresentação foi: {}'.format(lista))

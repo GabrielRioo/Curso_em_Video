6cc9ea6220ad6aed931775d288196856cedb1d14
GabrielRioo <Gabrielpaivario@gmail.com>
2020-05-23 22:52:36 -0300

    Adding the exercise 025

--- Curso_Python/Exercícios/ex025.py
+++ Curso_Python/Exercícios/ex025.py
@@ -1,2 +1,9 @@
 # Crie um programa que leia o nome de uma pessoa e diga se ela tem "SILVA" no nome
 # Em qualquer lugar tenha o silva. Retornar true ou false
+
+nome = str(input("Digite o seu nome: ")).strip().upper()
+print('SILVA' in nome, "\n", nome)
+
+#OU
+
+print('SILVA' in nome.upper())

b1fcd53f1c4c0e196d1b6aa91b523cdc1783b3d2
GabrielRioo <Gabrielpaivario@gmail.com>
2020-05-23 23:17:39 -0300

    Adding the exercise 027

--- Curso_Python/Exercícios/ex027.py
+++ Curso_Python/Exercícios/ex027.py
@@ -2,3 +2,6 @@
 # Exemplo: Ana Maria de Souza
 # primeiro: Ana
 # Ultimo: Souza
+
+nome = str(input('Digite um nome: ')).strip().split()
+print('Seu primeiro nome é {} e seu ultimo nome {}'.format(nome[0].capitalize(), nome[len(nome)-1].capitalize()))

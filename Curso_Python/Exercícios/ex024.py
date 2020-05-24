44b648da7c8ac190d604f66ee96829150d8acd20
GabrielRioo <Gabrielpaivario@gmail.com>
2020-05-23 22:47:02 -0300

    Adding the exercise 024

--- Curso_Python/Exercícios/ex024.py
+++ Curso_Python/Exercícios/ex024.py
@@ -1,1 +1,9 @@
-# Crie um progrAma que leia o nome de uma cidade  diga se ela começa oU nao com o nome 'SANTO'
+# Crie um programa que leia o nome de uma cidade  diga se ela começa ou nao com o nome 'SANTO'
+
+cidade = str(input('Digite o nome de uma cidade: ')).strip()
+cidade = cidade.lower()
+print('santo' in cidade)
+
+#OU
+
+print(cidade[:5].upper() == 'SANTO')

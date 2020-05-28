307b7c2d30ef1147e0593a7942d939065b2e21e2
GabrielRioo <Gabrielpaivario@gmail.com>
2020-05-28 14:28:41 -0300

    Adding the exercise 035

--- Curso_Python/Exercícios/ex035.py
+++ Curso_Python/Exercícios/ex035.py
@@ -1,5 +1,12 @@
 # Desenvolva um programa que leia o comprimeto de tres retas e diga ao usuario se elas podem ou nao formar um triangulo
 
-reta1 = float(input('Digite o comprimento da primeira reta '))
-reta2 = float(input('Digite o comprimento da segunda reta '))
-reta3 = float(input('Digite o comprimento da terceira reta '))
+print('-=-'*20)
+print('Analisador de Triangulos')
+print('-=-'*20)
+r1 = float(input('Digite o comprimento da primeira reta '))
+r2 = float(input('Digite o comprimento da segunda reta '))
+r3 = float(input('Digite o comprimento da terceira reta '))
+if r1 < r2 + r3 and r2 < r1 + r3:
+    print('Os segmentos podem formar um triangulo!')
+else:
+    print('Os segmentos nao podem formar um triangulo!')

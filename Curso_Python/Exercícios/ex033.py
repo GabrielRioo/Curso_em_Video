788056034e922d4d0aa182d38ce479a8858831d5
GabrielRioo <Gabrielpaivario@gmail.com>
2020-05-28 13:48:00 -0300

    Adding the exercise 033

--- Curso_Python/Exercícios/ex033.py
+++ Curso_Python/Exercícios/ex033.py
@@ -3,9 +3,23 @@
 n1 = int(input("Digite o primeiro numero: "))
 n2 = int(input('Digite o segundo numero: '))
 n3 = int(input('Digite o terceiro numero: '))
-if n1 > n2 and n1 > n3:
-    print('O maior numero é: {}'.format(n1))
+menor = n1
+if n2 < n1 and n2 < n3:
+    menor = n2
+if n3 < n1 and n3 < n2:
+    menor = n3
+
+maior = n1
 if n2 > n1 and n2 > n3:
-    print('O maior numero é: {}'.format(n2))
-else:
-    print('O maior numero é: {}'.format(n3))
+    maior = n2
+if n3 > n1 and n3 > n2:
+    maior = n3
+print('O maior numero é {} e o menor é {}'.format(maior, menor))
+
+#
+# if n1 > n2 and n1 > n3:
+#     print('O maior numero é: {}'.format(n1))
+# if n2 > n1 and n2 > n3:
+#     print('O maior numero é: {}'.format(n2))
+# else:
+#     print('O maior numero é: {}'.format(n3))
